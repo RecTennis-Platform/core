8611bc1fc7f9a97be2f2e841d3a8e92b88a5c6c7
hqkhai1636 <hqkhai1636@gmail.com>
2024-03-31 15:14:48 +0700

    ft: implement api get user's bought packages

--- src/bought_package/bought_package.controller.ts
+++ src/bought_package/bought_package.controller.ts
@@ -1,15 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { BoughtPackageService } from './bought_package.service';
 import { CreateBoughtPackageDto } from './dto/create-bought_package.dto';
 import { UpdateBoughtPackageDto } from './dto/update-bought_package.dto';
+import { JwtGuard } from 'src/auth_utils/guards';
+import { GetUser } from 'src/auth_utils/decorators';
 
-@Controller('bought-package')
+@Controller('bought-packages')
 export class BoughtPackageController {
   constructor(private readonly boughtPackageService: BoughtPackageService) {}
 
   @Post()
   create(@Body() createBoughtPackageDto: CreateBoughtPackageDto) {
     return this.boughtPackageService.create(createBoughtPackageDto);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('/me')
+  async addUserToGroup(@GetUser('sub') userId: number) {
+    try {
+      return await this.boughtPackageService.getUserBoughtPackages(userId);
+    } catch (error) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: error.message || 'Internal Server Error',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Get()
@@ -23,7 +52,10 @@ export class BoughtPackageController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBoughtPackageDto: UpdateBoughtPackageDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateBoughtPackageDto: UpdateBoughtPackageDto,
+  ) {
     return this.boughtPackageService.update(+id, updateBoughtPackageDto);
   }
 
--- src/bought_package/bought_package.service.ts
+++ src/bought_package/bought_package.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBoughtPackageDto } from './dto/create-bought_package.dto';
 import { UpdateBoughtPackageDto } from './dto/update-bought_package.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { MongoDBPrismaService } from 'src/prisma/prisma.mongo.service';
 
 @Injectable()
 export class BoughtPackageService {
+  constructor(
+    private prismaService: PrismaService,
+    private readonly mongodbPrismaService: MongoDBPrismaService,
+  ) {}
   create(createBoughtPackageDto: CreateBoughtPackageDto) {
     return 'This action adds a new boughtPackage';
   }
@@ -22,5 +28,33 @@ export class BoughtPackageService {
 
   remove(id: number) {
     return `This action removes a #${id} boughtPackage`;
+  }
+
+  async getUserBoughtPackages(userId: number) {
+    const packages = await this.mongodbPrismaService.boughtPackage.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return packages.map((value) => {
+      return {
+        id: value.id,
+        userId: value.userId,
+        expired: value.expired,
+        orderId: value.orderId,
+        startDate: value.startDate,
+        endDate: value.endDate,
+        name: value.package.name,
+        services: value.package.services.map((service) => {
+          const { config, ...serviceInfo } = service;
+          const configValue = JSON.parse(config);
+          return {
+            ...serviceInfo,
+            ...configValue,
+          };
+        }),
+      };
+    });
   }
 }
