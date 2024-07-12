1b7eb42401eeb21bb859bb162531d4f9890c1033
hqkhai1636 <hqkhai1636@gmail.com>
2024-07-12 11:42:13 +0700

    improve news crawler

--- src/services/jobs/news-crawler.ts
+++ src/services/jobs/news-crawler.ts
@@ -7,16 +7,18 @@ const NewsAPI = require('newsapi');
 export class NewsCrawlerService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  @Cron(CronExpression.EVERY_2_HOURS)
+  @Cron(CronExpression.EVERY_DAY_AT_1PM)
   async handleCron() {
     const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
     const now = new Date();
     const toDate = now.toISOString();
-    const fromDate = new Date(now.getTime() - 2 * 60 * 60 * 1000).toISOString();
+    const fromDate = new Date(
+      now.getTime() - 24 * 60 * 60 * 1000 * 2,
+    ).toISOString();
+    console.log(toDate, fromDate);
     newsapi.v2
       .everything({
         q: 'tennis',
-        sources: 'bbc-sport',
         from: fromDate,
         to: toDate,
         language: 'en',
@@ -25,6 +27,7 @@ export class NewsCrawlerService {
       })
       .then(async (response) => {
         if (response.status === 'ok') {
+          console.log(response.articles);
           const news = (
             await Promise.all(
               response.articles.map(async (article) => {
@@ -41,6 +44,7 @@ export class NewsCrawlerService {
               }),
             )
           ).filter(Boolean);
+          console.log(news);
           await this.prismaService.news.createMany({
             data: news,
           });
