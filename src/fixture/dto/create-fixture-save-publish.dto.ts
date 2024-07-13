49be81a87b50de2dcff256f89b33cacf4f3ddddf
hqkhai1636 <hqkhai1636@gmail.com>
2024-07-13 10:56:08 +0700

    add field validation for group

--- src/fixture/dto/create-fixture-save-publish.dto.ts
+++ src/fixture/dto/create-fixture-save-publish.dto.ts
@@ -16,6 +16,7 @@ import {
 export class Group {
   @IsNotEmpty()
   @IsArray()
+  @ValidateNested({ each: true })
   teams: string[];
 
   @IsOptional()
--- src/fixture/dto/create-fixture.dto.ts
+++ src/fixture/dto/create-fixture.dto.ts
@@ -17,6 +17,7 @@ import {
 export class Group {
   @IsNotEmpty()
   @IsArray()
+  @ValidateNested({ each: true })
   teams: string[];
 
   @IsOptional()
